bbff23de0b3d1e5995daaa53406c85149f04cd7b
zperd <zperd@statesboro>
2017-04-01 16:40:11 -0400

    partial implementation of displaying the numbers

--- zperdGit/src/zperdGit/SalesData.java
+++ zperdGit/src/zperdGit/SalesData.java
@@ -4,6 +4,8 @@ public class SalesData {
 	int data[] = {0, 4, 42};
 	
 	public void Display(){
-		System.out.println("Sales Data");
+		for(int i =0; i< data.length; i++){
+			System.out.println("");
+		}
 	}
 }
