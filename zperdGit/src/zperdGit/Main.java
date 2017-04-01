d4b7ddfd2eba95ab497e09601b2b603c7204a7a7
zperd <zperd@statesboro>
2017-04-01 15:52:48 -0400

    Initialized

---
+++ zperdGit/src/zperdGit/Main.java
@@ -0,0 +1,16 @@
+package zperdGit;
+
+public class Main {
+	public static void main(String args[]){
+	
+	SalesData sales = new SalesData();
+	displayGreeting();
+	sales.Display();
+	
+	}
+	private static void displayGreeting(){
+		System.out.println("Hello");
+		System.out.println("We sell knowledge");
+
+	}
+}
